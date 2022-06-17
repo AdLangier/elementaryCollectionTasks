b36bcb958acedd38d58ae1db6b69293c09f802de
Adrian Langier <langier.adrian@gmail.com>
2022-06-17 20:45:52 +0200

    comment addition

--- src/com/company/CollectionInit.java
+++ src/com/company/CollectionInit.java
@@ -7,7 +7,7 @@ public class CollectionInit {
     static Scanner scanner = new Scanner(System.in);
 
 
-    static List<Integer> intArrayListRead(List<Integer> numbers) {
+    static List<Integer> intArrayListRead(List<Integer> numbers) {          // metoda tworzy Listę liczb podanych przez użytkownika
         System.out.println("Podaj wielkość Listy liczb całkowitych");
         int n = scanner.nextInt();
         System.out.println("Podaj elementy Listy (liczby całkowite)");
@@ -18,7 +18,7 @@ public class CollectionInit {
         return userList;
     }
 
-    static List<String> stringArrayListRead(List<String> words) {
+    static List<String> stringArrayListRead(List<String> words) {           // metoda tworzy Listę wyrazów podanych przez użytkownika
         System.out.println("Podaj wielkość Listy wyrazów");
         int n = scanner.nextInt();
         System.out.println("Podaj elementy Listy (wyrazy)");
@@ -29,9 +29,9 @@ public class CollectionInit {
         return userList;
     }
 
-    static Map<String, String> dictionaryInput(Map<String, String> map) {
-        Map<String, String> dictionary = new TreeMap<>();
-        System.out.println("Wprowadź słowo po polsku, po enterze tłumaczenie");
+    static Map<String, String> dictionaryInput(Map<String, String> map) {           // metoda prosząca o uzupełnienie słownika przez użytkownika,
+        Map<String, String> dictionary = new TreeMap<>();                           // wprowadzamy słowo po polsku, następnie po enterze słowo po angielsku
+        System.out.println("Wprowadź słowo po polsku, po enterze tłumaczenie");     // po wpisaniu "end /enter "input" - zakończymy dodawanie słów
 
         String polWord;
         String foreignWord;
